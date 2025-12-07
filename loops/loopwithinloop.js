d4d90c6e34ae9ab7d1abf81c1db6bec10be3d227
JenishAgravat <jenishagravat04@gmail.com>
2025-12-07 16:01:59 +0530

    star patterns

---
+++ loops/loopwithinloop.js
@@ -0,0 +1,6 @@
+//How loops within loops
+for(let i=0;i<3;i++){
+    for(let j=0;j<3;j++){
+        console.log("i="+i+" j="+j);
+    }
+}
---
+++ loops/pyramids.js/patterns.js
@@ -0,0 +1,88 @@
+// let n=4;
+// for(let i=0;i<n;i++){                           //**** 
+//     let row=" ";                                //**** 
+//     for(let j=0;j<n;j++){                      //**** 
+//         row +="*";                             //****
+//     }
+//     console.log(row);
+// }
+
+// let n=4;
+// for(let i=0;i<n;i++){                            //*
+//      row="";                                     //* *
+//     for(let j=0;j<=i;j++){                      //* * *
+//          row+=" *";                             //* * * *
+//     }
+//     console.log(row);
+// }
+
+// let n=5;
+// for(let i=0;i<n;i++){
+//     let row="";                                
+//     for(let j=0;j<=i;j++){
+//         row+=j+1;
+//     }
+//     console.log(row);
+// }
+
+// let n=5;
+// for(let i=0;i<n;i++){
+//     row="";
+//     for(let j=0;j<=i;j++){
+//         row+=i+1;
+//     }
+//     console.log(row);
+// }
+
+// let n=5;
+// for(let i=0;i<n;i++){
+//     let row="";
+//     for(let j=0;j<n-i;j++){
+//         row+=j+1
+//     }
+//     console.log(row);
+// }
+
+// let n=5;
+// for(let i=0;i<n;i++){
+//     let row="";
+//     for(let j=0;j<n-(i+1);j++){
+//         row+=" ";
+//     }
+//     for(let k=0;k<i+1;k++){
+//         row+="*";
+//     }
+//     console.log(row); 
+// }
+
+// let n=5;
+// for(let i=0;i<n;i++){
+//     let row="";
+//     let toggle=1;
+//     for(let j=0;j<=i;j++){
+//         row+=toggle;
+//         if(toggle==1){
+//             toggle=0;
+//         }
+//         else{
+//             toggle=1;
+//         }
+//     }
+//     console.log(row); 
+// }
+
+let n=5;
+let toggle=1;
+for(let i=0;i<n;i++){
+    let row="";
+    for(let j=0;j<=i;j++){
+        row+=toggle;
+        if(toggle==1){
+            toggle=0;
+        }
+        else{
+            toggle=1;
+        }
+    }
+    console.log(row); 
+}
