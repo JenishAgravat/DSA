79891606ec3448f5b8793f6e37e713c2b68b0868
JenishAgravat <jenishagravat04@gmail.com>
2025-12-11 15:06:42 +0530

    max consecutive ones

---
+++ loops/max_consecutive_ones.js
@@ -0,0 +1,14 @@
+var findMaxConsecutiveOnes = function(nums) {
+    let currentCount = 0;
+    let maxCount = 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] == 1){
+            currentCount ++;
+        }
+        else{
+            maxCount = Math.max(maxCount, currentCount);
+            currentCount = 0;
+        }
+    }
+    return Math.max(maxCount, currentCount);
+};
