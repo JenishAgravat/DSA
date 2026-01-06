c2b274dddf87212b0ad5c17074aad7985a5e704a
JenishAgravat <jenishagravat04@gmail.com>
2026-01-06 22:18:01 +0530

    lineat_search

---
+++ Part_5/LinerarSearch.js
@@ -0,0 +1,13 @@
+let arr=[2,3,4,5,6]
+
+function linearSearch(arr,target){
+for(let i=0;i<arr.length;i++){
+    if(arr[i]==target){
+        return i;
+    }
+}
+return -1;
+}
+let result =linearSearch(arr,0);
+console.log(result);
+
